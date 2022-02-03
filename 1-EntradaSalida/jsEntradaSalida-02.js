8780192cfa52b0a21355c61ef50be2d0f279411d
jorgeGAB <jorgealvarenga809@gmail.com>
2022-02-03 16:13:51 +0100

    Actualizacion clase 03/02/2022

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -1,8 +1,25 @@
+/*Jorge gabriel Alvarenga Britez
+Ejercicio 2*/
 /*
 Debemos lograr tomar un nombre con 'prompt' 
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
-{
-	alert("ok");
+{ /* para declarar variables se usa la palabra reservada let*/
+	
+	/*estoy delcarando variables*/
+	let nombres;
+
+	/*asignacion*/
+     
+	  /*esto es una asignacion con =*/
+	  /* el prompt me permetite pedir datos por teclado*/
+
+        nombre = prompt("Ingrese su nombre");
+
+	
+ 	  /*estoy motrando un cartel con alert*/
+	alert(nombre);
+	
+	/*alert("ok");*/
 }
 
