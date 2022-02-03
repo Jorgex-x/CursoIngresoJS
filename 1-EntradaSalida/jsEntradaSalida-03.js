47af96adef64fe8a05e3fcfe08a8f04f27a9a0eb
jorgeGAB <jorgealvarenga809@gmail.com>
2022-02-03 17:29:05 +0100

    Clase 03/02/2022 :)

--- 1-EntradaSalida/jsEntradaSalida-03.js
+++ 1-EntradaSalida/jsEntradaSalida-03.js
@@ -5,10 +5,15 @@ Debemos lograr tomar un dato por 'ID'
 y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'*/
 
 function mostrar()
-{
+{ /*txtIdNombre (nombre de la caja que esta en la pagina)*/
+	
 	let nombreIngresado;
-	nombreIngresado=txtIdNombre.value;
-	alert("ok");
+	
+	/*nombreIngresado=txtIdNombre.value; dieñado para chrome, no es estandar de Javascript (No realizarlo porque no es conrrecto, es un punto menos para los profes)*/
+
+	nombreIngresado = document.getElementById("txtIdNombre").value;
+	
+	alert(nombreIngresado);
 
 }
 
