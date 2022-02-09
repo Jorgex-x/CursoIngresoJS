aaa2310562933a504526a67d746bd29f9e075431
jorgeGAB <jorgealvarenga809@gmail.com>
2022-02-09 11:47:40 +0100

    Ejercicio 7 resuelto

--- 1-EntradaSalida/jsEntradaSalida-07.js
+++ 1-EntradaSalida/jsEntradaSalida-07.js
@@ -1,3 +1,5 @@
+/*Jorge Gabriel Alvarenga Britez
+Ejercicio 7*/
 /*
 Debemos lograr tomar Los numeros por ID ,
 transformarlos a enteros (parseInt),realizar la operación correcta y 
@@ -5,21 +7,67 @@ mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
 {	
-	alert("ok");	
+	let num1;
+	let num2;
+	let resultado;
+
+	num1 = parseInt(document.getElementById("txtIdNumeroUno").value);
+
+	num2 = parseInt(document.getElementById("txtIdNumeroDos").value);
+
+	resultado = num1 + num2;
+		
+	alert("La suma es " + resultado);
+
 }
 
 function restar()
 {
 	
+	let num1;
+	let num2;
+	let resultado;
+
+	num1 = parseInt(document.getElementById("txtIdNumeroUno").value);
+
+	num2 = parseInt(document.getElementById("txtIdNumeroDos").value);
+
+	resultado = num1 - num2;
+	
+	alert("La resta es " + resultado);
+
 }
 
 function multiplicar()
 { 
+
+	let num1;
+	let num2;
+	let resultado;
+
+	num1 = parseInt(document.getElementById("txtIdNumeroUno").value);
+
+	num2 = parseInt(document.getElementById("txtIdNumeroDos").value);
 	
+	resultado = num1 * num2;
+
+	alert("el producto es " + resultado);
+
 }
 
 function dividir()
 {
 	
-}
+	let num1;
+	let num2;
+	let resultado;
+
+	num1 = parseInt(document.getElementById("txtIdNumeroUno").value);
+
+	num2 = parseInt(document.getElementById("txtIdNumeroDos").value);
+
+	resultado = num1 / num2;
+
+	alert("La cociente es " + resultado);
+} 
 
