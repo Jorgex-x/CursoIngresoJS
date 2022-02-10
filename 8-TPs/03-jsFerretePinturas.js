99ca2f94db75081b5d935a2d8d2fa034f0da8161
jorgeGAB <jorgealvarenga809@gmail.com>
2022-02-10 21:17:43 +0100

    Tp 3 realizado

--- 8-TPs/03-jsFerretePinturas.js
+++ 8-TPs/03-jsFerretePinturas.js
@@ -4,10 +4,32 @@ B.	Al ingresar una temperatura en Centígrados debemos mostrar la temperatura en
 */
 function FahrenheitCentigrados () 
 {
-	
+    let gradosFahrenheit
+    let gradosCelsius
+    let celsius
+    const deFahrenheitACelsius = 5/9 
+
+    gradosCelsius = parseFloat(document.getElementById("txtIdTemperatura").value);
+
+    gradosFahrenheit = parseFloat(document.getElementById("txtIdTemperatura").value);
+
+    celsius = (gradosFahrenheit - 32) * deFahrenheitACelsius;
+
+    alert (gradosFahrenheit + "° F son " + celsius.toFixed(2) + "° C" )
 }
 
 function CentigradosFahrenheit () 
 {
-	
+    let gradosFahrenheit
+    let gradosCelsius
+    let fahrenheit
+    const deCelsiusAFahrenheit = 9/5
+
+    gradosCelsius = parseFloat(document.getElementById("txtIdTemperatura").value);
+
+    gradosFahrenheit = parseFloat(document.getElementById("txtIdTemperatura").value);
+
+    fahrenheit = (gradosCelsius * deCelsiusAFahrenheit) +32;
+
+    alert (gradosCelsius + "° C son " + fahrenheit.toFixed(2) + " °F")
 }
